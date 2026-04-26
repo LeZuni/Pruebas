4ca3d7199d1b3614fc5a6f04a801f141aec3d194
LeZuni <leslymendez896@gmail.com>
2026-04-25 23:52:00 -0600

    feat: :sparkles: add listar productos

--- inventarios.py
+++ inventarios.py
@@ -13,3 +13,14 @@ def agregar_producto():
     
     inventario[nombre] = {"precio": precio, "cantidad": cantidad}
     print("Producto agregado correctamente.")
+def listar_productos():
+    if not inventario:
+        print("El inventario está vacío.")
+        return
+    
+    print("\n--- Lista de productos ---")
+    for nombre, datos in inventario.items():
+        print(f"Producto: {nombre}")
+        print(f"Precio: Q{datos['precio']}")
+        print(f"Cantidad: {datos['cantidad']}")
+        print("-------------------------")
