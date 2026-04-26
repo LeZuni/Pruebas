216c1146ba405bfad8b0fa462940abb864e098f1
LeZuni <leslymendez896@gmail.com>
2026-04-25 23:24:10 -0600

    feat: :sparkles: add agregar producto

---
+++ inventarios.py
@@ -0,0 +1,15 @@
+# Inventario: {nombre: {"precio": float, "cantidad": int}}
+inventario = {}
+
+def agregar_producto():
+    nombre = input("Nombre del producto: ").lower()
+    
+    if nombre in inventario:
+        print("El producto ya existe.")
+        return
+    
+    precio = float(input("Precio: "))
+    cantidad = int(input("Cantidad: "))
+    
+    inventario[nombre] = {"precio": precio, "cantidad": cantidad}
+    print("Producto agregado correctamente.")
