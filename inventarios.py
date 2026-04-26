f9453b1696162295cacfb804b97cbd043a91e591
LeZuni <leslymendez896@gmail.com>
2026-04-26 00:05:40 -0600

    feat: :bug: add actualizar cantidad

--- inventarios.py
+++ inventarios.py
@@ -24,3 +24,13 @@ def listar_productos():
         print(f"Precio: Q{datos['precio']}")
         print(f"Cantidad: {datos['cantidad']}")
         print("-------------------------")
+def actualizar_cantidad():
+    nombre = input("Nombre del producto a actualizar: ").lower()
+    
+    if nombre not in inventario:
+        print("El producto no existe.")
+        return
+    
+    nueva_cantidad = int(input("Nueva cantidad: "))
+    inventario[nombre]["cantidad"] = nueva_cantidad
+    print("Cantidad actualizada.")
